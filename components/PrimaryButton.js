4be61dda4da0ce5d57959141d3d60f888ea05ae3
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-10 15:10:16 +0700

    style: styling botton for ios

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -7,7 +7,11 @@ function PrimaryButton({ children }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        style={styles.buttonInnerContainer}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
         onPress={pressHandler}
         android_ripple={{ color: "#640233" }}
       >
@@ -36,5 +40,8 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
